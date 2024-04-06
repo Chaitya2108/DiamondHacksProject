576cc88ce149da39313529861886841dda2c1566
Chaitya Jodhavat <chaityajodhavat@gmail.com>
2024-04-06 16:34:15 -0700

    hi

--- src/app/student/page.tsx
+++ src/app/student/page.tsx
@@ -1,39 +1,25 @@
-'use client';
+import { TopNavbar } from "../_components/TopNavbar";
+import { SideNavbar } from "../_components/SideNavbar";
+import { QuizEdit } from "../_components/QuizEdit";
 
-import Button from '../_components/button';
-import React, { use, useState } from 'react';
-
-export default function Student() {
-    const[openedEditor, setOpenedEditor] = useState("html");
-
-    const onTabClick = (editorName: any) => {
-        setOpenedEditor(editorName);
-    }
-    return (
-        <div className="App">
-      <p>Welcome to the editor!</p>
-      <div className="tab-button-container flex">
-        <Button title="HTML" onClick={() => {
-          onTabClick('html')
-        }} />
-        <Button title="CSS" onClick={() => {
-          onTabClick('css')
-        }} />
-        <Button title="JavaScript" onClick={() => {
-          onTabClick('js')
-        }} />
-      </div>
-      <div className="editor-container">
-        {
-          openedEditor === 'html' ? (
-            <p>The html editor is open</p>
-          ) : openedEditor === 'css' ? (
-            <p>The CSS editor is open!!!!!!</p>
-          ) : (
-            <p>the JavaScript editor is open</p>
-          )
-        }
+export default function Student({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div className="flex col" style={{width: "100vw", height: "100vh"}}>
+    <div className="w-screen">
+        <TopNavbar />
+    </div>
+    <div className='flex' style={{height: "100%"}}>
+        <div style={{width: "40%"}}>
+            <SideNavbar />
+        </div>     
+        <QuizEdit />
       </div>
     </div>
-    )
+    
+    
+  );
 }
